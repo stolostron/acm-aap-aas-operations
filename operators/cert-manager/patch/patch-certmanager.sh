a25e525a188eed68ddfb594904f84cf8f5c54629
Gurney Buchanan <gurney.buchanan@gmail.com>
2021-12-15 11:04:57 -0500

    Fixing patch script, updating subscription and application for cert-manager

--- operators/cert-manager/patch/patch-certmanager.sh
+++ operators/cert-manager/patch/patch-certmanager.sh
@@ -2,4 +2,5 @@
 
 oc patch clusterserviceversion cert-manager.v1.6.1 \
     --type=json \
+    -n openshift-operators \
     --patch='[{"op":"add","path":"/spec/install/spec/deployments/0/spec/template/spec/containers/0/args/-","value":"--dns01-recursive-nameservers-only"},{"op":"add","path":"/spec/install/spec/deployments/0/spec/template/spec/containers/0/args/-","value":"--dns01-recursive-nameservers=8.8.8.8:53,1.1.1.1:53"}]'
