afd1c65b5c8edeaea38749801c42b6fa960a6496
Gurney Buchanan <gurney.buchanan@gmail.com>
2021-12-15 10:45:02 -0500

    Update subscriptions and add patch

---
+++ operators/cert-manager/patch/patch-certmanager.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+oc patch clusterserviceversion cert-manager.v1.6.1 \
+    --type=json \
+    --patch='[{"op":"add","path":"/spec/install/spec/deployments/0/spec/template/spec/containers/0/args/-","value":"--dns01-recursive-nameservers-only"},{"op":"add","path":"/spec/install/spec/deployments/0/spec/template/spec/containers/0/args/-","value":"--dns01-recursive-nameservers=8.8.8.8:53,1.1.1.1:53"}]'
