1612e82f8049ea1a4c8fb0787eced74243b39782
Zachary Kayyali <zkayyali@redhat.com>
2022-04-08 13:16:49 -0400

    Update patch-certmanager.sh
    Signed-off-by: Zachary Kayyali <zkayyali@redhat.com>

--- operators/cert-manager/patch/patch-certmanager.sh
+++ operators/cert-manager/patch/patch-certmanager.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-oc patch clusterserviceversion cert-manager.v1.6.1 \
+_CSV_NAME=$(oc get sub -n openshift-operators cert-manager -ojsonpath='{.status.currentCSV}')
+
+oc patch clusterserviceversion ${_CSV_NAME} \
     --type=json \
     -n openshift-operators \
     --patch='[{"op":"add","path":"/spec/install/spec/deployments/0/spec/template/spec/containers/0/args/-","value":"--dns01-recursive-nameservers-only"},{"op":"add","path":"/spec/install/spec/deployments/0/spec/template/spec/containers/0/args/-","value":"--dns01-recursive-nameservers=8.8.8.8:53,1.1.1.1:53"}]'
