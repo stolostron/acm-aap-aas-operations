0310902bc45f1349a1904a4ddda30c3b3aea130a
Qian <qiding@redhat.com>
2022-04-15 20:52:37 +0800

    updates

--- scripts/patch-certmanager.sh
+++ scripts/patch-certmanager.sh
@@ -2,6 +2,10 @@
 
 _CSV_NAME=$(oc get sub -n openshift-operators cert-manager -ojsonpath='{.status.currentCSV}')
 
+while [[ $(oc get clusterserviceversion ${_CSV_NAME} --no-headers --ignore-not-found | wc -l) -eq 0 ]]; 
+    do echo "Waiting for csv to be created" && sleep 30;
+done
+
 oc patch clusterserviceversion ${_CSV_NAME} \
     --type=json \
     -n openshift-operators \
