09a2209f6ba06f73f501cc10c2c18db4a2c0b066
Qian <qiding@redhat.com>
2022-04-15 09:53:25 +0800

    Add cert-manager

---
+++ cluster-bootstrap/cert-manager/base/patch-certmanager.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+_CSV_NAME=$(oc get sub -n openshift-operators cert-manager -ojsonpath='{.status.currentCSV}')
+
+oc patch clusterserviceversion ${_CSV_NAME} \
+    --type=json \
+    -n openshift-operators \
+    --patch='[{"op":"add","path":"/spec/install/spec/deployments/0/spec/template/spec/containers/0/args/-","value":"--dns01-recursive-nameservers-only"},{"op":"add","path":"/spec/install/spec/deployments/0/spec/template/spec/containers/0/args/-","value":"--dns01-recursive-nameservers=8.8.8.8:53,1.1.1.1:53"}]'
